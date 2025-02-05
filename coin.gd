185c398d2499eb5d26dd7f93be33e953c2d93cf3
Carolxnexxx <145205914+Carolxnexxx@users.noreply.github.com>
2025-02-04 19:48:06 -0500

    Update coin.gd

--- coin.gd
+++ coin.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-
+@onready var game_manager: Node = %GameManager
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1") # Replace with function body.
+	game_manager.add_point()
+	audio_player.reparent(get_tree().current_scene)
+	audio_player.play()
 	queue_free()
