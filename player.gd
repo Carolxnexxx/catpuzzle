fee587d95430c3f6e01ed97bec9a53bcc166e787
Carolxnexxx <145205914+Carolxnexxx@users.noreply.github.com>
2025-02-11 21:07:53 -0500

    Update player.gd

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_cat_audio: AudioStreamPlayer2D = $JumpCatAudio
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +13,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_cat_audio.reparent(get_tree().current_scene)
+		jump_cat_audio.play()
 
 	# Get input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
